661cb800ee014452411c48b98e0e72438c3f09d4
ckpunmkug <ckpunmkug@yandex.ru>
2017-04-19 07:16:11 +0000

    Add files via upload

--- text/background.php
+++ text/background.php
@@ -1,15 +1,14 @@
 <?php
-$width = 802;
-$image = imagecreate( $width, 1 );
+$image = imagecreate( 1, 3 );
 if( is_resource( $image ) )
 	{
-	$background = imagecolorallocate( $image, 0x00, 0x00, 0x00 );
-	$pixel = imagecolorallocate( $image, 0x00, 0x10, 0x00 );
-	for( $x = 0; $x < ( $width - 1 ); $x++ )
-		imagesetpixel( $image, $x, 0, $background );
-	imagesetpixel( $image, ( $width - 1 ), 0, $pixel );
-	header( 'Content-Type: image/png' );
-	imagepng( $image );
-	imagedestroy( $image );
+	$top = imagecolorallocate( $image, 0x00, 0x00, 0x00 );
+	$bottom = imagecolorallocate( $image, 0x00, 0x10, 0x00 );
+	imagesetpixel( $image, 0, 0, $top );
+	imagesetpixel( $image, 0, 1, $bottom );
+	imagesetpixel( $image, 0, 2, $bottom );
+	header('Content-Type: image/png');
+	imagepng($image);
+	imagedestroy($image);
 	exit(  );
 	}
